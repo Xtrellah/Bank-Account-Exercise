fdf9e2ca9c8b2e6e119115bc4cc7152c19412ea6
Max Estrella <maxestrella565@gmail.com>
2024-07-11 10:25:28 +0100

    Bank Account class

--- index.php
+++ index.php
@@ -12,17 +12,33 @@ class BankAccount {
         {
             throw new Exception('You cannot open an account with no balance');
         }
-        return "Your balance is $balance";
+    }
+
+    public function deposit($deposit) {
+        if ($deposit > 0)
+        {
+            return $this->balance + $deposit;
+        }
+        return $this->balance + $deposit;
     }
 
     public function getBalance() {
-        $this->getBalance = $getBalance;
-        {
-            return "Your balance is $balance";
-        }
+        return "Your balance is $this->balance";
+    }
+
+    protected function calculateInterest() {
+        return $this->balance * 0.05;
+    }
+
+    public function applyInterest() {
+        return $this->balance + $this->calculateInterest();
     }
 }
 
 $BankAccount = new BankAccount(100);
+$BankAccount->deposit(10.5);
+$BankAccount->getBalance();
+$BankAccount->calculateInterest();
+$BankAccount->applyInterest();
 
-echo $balance;
+var_dump($BankAccount);
